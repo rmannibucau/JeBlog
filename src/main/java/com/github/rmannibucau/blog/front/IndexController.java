95b8ef2b1aba3fbf435819a5991c9d0e4806ccf0
Romain Manni-Bucau <rmannibucau@apache.org>
2013-03-19 21:43:12 +0100

    basic home page

--- src/main/java/com/github/rmannibucau/blog/front/IndexController.java
+++ src/main/java/com/github/rmannibucau/blog/front/IndexController.java
@@ -3,9 +3,7 @@ package com.github.rmannibucau.blog.front;
 import com.github.rmannibucau.blog.dao.PostDao;
 import com.github.rmannibucau.blog.dao.Repository;
 import com.github.rmannibucau.blog.domain.Post;
-import org.primefaces.model.LazyDataModel;
-import org.primefaces.model.SortMeta;
-import org.primefaces.model.SortOrder;
+import org.apache.deltaspike.core.api.config.annotation.ConfigProperty;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -14,45 +12,64 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.Arrays;
+import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
 @Named
 @ViewScoped
-public class IndexController extends LazyDataModel<Post> {
-    private static final int DEFAULT_PAGE_SIZE = 15;
-
+public class IndexController implements Serializable {
     @Inject
     @Repository
     private PostDao posts;
 
+    @Inject
+    @ConfigProperty(name = "jeblog.page-size", defaultValue = "15")
+    private Integer pageSize;
+
+    private int pageIndex = 0;
+    private Page<Post> page;
+
     @PostConstruct
-    public void initModel() {
-        setPageSize(DEFAULT_PAGE_SIZE);
+    public void init() {
+        setPage(0); // force page to be != null
     }
 
-    @Override
-    public List<Post> load(final int first, final int pageSize, final List<SortMeta> ignored1, final Map<String, String> ignored2) {
-        final PageRequest pageable = new PageRequest(first / pageSize, pageSize,
-                                                    new Sort(Sort.Direction.DESC, "created"));
-        final Page<Post> page = posts.findByStatus(Post.Status.PUBLISHED, pageable);
-        setRowCount(Long.valueOf(page.getTotalElements()).intValue());
+    public List<Post> getPosts() {
         return page.getContent();
     }
 
-    @Override
-    public List<Post> load(final int first, final int pageSize, final String sortField, final SortOrder sortOrder, final Map<String,String> filters) {
-        return load(first, pageSize, Arrays.asList(new SortMeta(null, sortField, sortOrder, null)), filters);
+    public void nextPage() {
+        setPage(++pageIndex);
     }
 
-    @Override
-    public Post getRowData(final String rowKey) {
-        return posts.findOne(Long.parseLong(rowKey));
+    public void previousPage() {
+        setPage(Math.max(0, --pageIndex));
     }
 
-    @Override
-    public Object getRowKey(final Post object) {
-        return object.getId().toString();
+    public void beginning() {
+        setPage(0);
+    }
+
+    public void end() {
+        setPage(page.getTotalPages() - 1);
+    }
+
+    public boolean getHasPrevious() {
+        return page.hasPreviousPage();
+    }
+
+    public boolean getHasNext() {
+        return page.hasNextPage();
+    }
+
+    public int getCurrent() {
+        return page.getNumber() + 1;
+    }
+
+    private void setPage(final int idx) {
+        pageIndex = idx;
+
+        final PageRequest pageable = new PageRequest(idx, pageSize, new Sort(Sort.Direction.DESC, "created"));
+        page = posts.findByStatus(Post.Status.PUBLISHED, pageable);
     }
 }
--- src/main/java/com/github/rmannibucau/blog/init/FirstStartup.java
+++ src/main/java/com/github/rmannibucau/blog/init/FirstStartup.java
@@ -4,11 +4,13 @@ import com.github.rmannibucau.blog.dao.PostDao;
 import com.github.rmannibucau.blog.dao.UserDao;
 import com.github.rmannibucau.blog.domain.Post;
 import com.github.rmannibucau.blog.domain.User;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import java.util.Random;
 
 @Singleton
 @Startup
@@ -32,12 +34,15 @@ public class FirstStartup {
     @Inject
     private PostDao posts;
     private void tmp() {
+        final Random random = new Random();
         for (int i = 0; i < 100; i++) {
             final Post post = new Post();
             post.setAuthor(users.findAll().iterator().next());
-            post.setContent("bla " + i);
+            for (int j = 0; j < 100; j++) {
+                post.setContent(post.getContent() + " " + RandomStringUtils.randomAlphanumeric(random.nextInt(50)));
+            }
             post.setStatus(Post.Status.PUBLISHED);
-            post.setTitle("p_" + i);
+            post.setTitle("Title #" + i);
             posts.save(post);
         }
     }
