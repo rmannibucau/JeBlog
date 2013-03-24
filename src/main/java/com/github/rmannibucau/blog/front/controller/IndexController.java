1d8ed7a2d69e3481f15a2bf1f2046641031b410c
Romain Manni-Bucau <rmannibucau@apache.org>
2013-03-24 01:02:38 +0100

    basic hack to support tag on index

--- src/main/java/com/github/rmannibucau/blog/front/controller/IndexController.java
+++ src/main/java/com/github/rmannibucau/blog/front/controller/IndexController.java
@@ -2,7 +2,9 @@ package com.github.rmannibucau.blog.front.controller;
 
 import com.github.rmannibucau.blog.dao.PostDao;
 import com.github.rmannibucau.blog.dao.Repository;
+import com.github.rmannibucau.blog.dao.TagDao;
 import com.github.rmannibucau.blog.domain.Post;
+import com.github.rmannibucau.blog.domain.Tag;
 import com.github.rmannibucau.blog.front.dto.PostDto;
 import com.github.rmannibucau.blog.processor.ContentProcessor;
 import org.apache.commons.lang3.StringUtils;
@@ -13,10 +15,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +33,10 @@ public class IndexController implements Serializable {
     private PostDao posts;
 
     @Inject
+    @Repository
+    private TagDao tags;
+
+    @Inject
     @ConfigProperty(name = "jeblog.page-size", defaultValue = "15")
     private Integer pageSize;
 
@@ -39,11 +47,14 @@ public class IndexController implements Serializable {
     @Inject
     private ContentProcessor processor;
 
+    @Inject
+    private EntityManager em;
+
     private int pageIndex = 0;
     private Page<PostDto> page;
     private String tag;
 
-    @PostConstruct
+    // @PostConstruct // need viewParam so don't use cdi here
     public void init() {
         setPage(0); // force page to be != null
     }
@@ -85,8 +96,26 @@ public class IndexController implements Serializable {
 
         final PageRequest pageable = new PageRequest(idx, pageSize, new Sort(Sort.Direction.DESC, "created"));
 
-        final Page<Post> queryResult = posts.findByStatus(Post.Status.PUBLISHED, pageable);
-        // TODO: handle tag
+        final Page<Post> queryResult;
+        if (tag == null) {
+            queryResult = posts.findByStatus(Post.Status.PUBLISHED, pageable);
+        } else {
+            final Tag tagValue = tags.findByName(tag);
+            if (tagValue == null) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Can't find tag " + tag, null));
+                queryResult = posts.findByStatus(Post.Status.PUBLISHED, pageable);
+            } else {
+                queryResult = new PageImpl<>(
+                        em.createQuery("select p from Post p where :tag member of p.tags and p.status = :status", Post.class)
+                            .setParameter("status", Post.Status.PUBLISHED)
+                            .setParameter("tag", tagValue).getResultList(),
+                        pageable,
+                        em.createQuery("select count(p) from Post p where :tag member of p.tags and p.status = :status", Long.class)
+                            .setParameter("status", Post.Status.PUBLISHED)
+                            .setParameter("tag", tagValue).getSingleResult());
+            }
+        }
+
 
         page = new PageImpl<>(previewContent(queryResult), pageable, queryResult.getTotalElements());
     }
