fd895ceb8e7d3c7e894c315f0fe6c8b4b4b03b51
Dan Allen <dallen@redhat.com>
2013-05-09 03:27:35 -0600

    instructions, source highlighting, edits
    - add basic instructions for running the application
    - enable source highlighting using CodeRay
    - add styles from default CodeRay theme
    - fix spelling of Markdown
    - fix case of AsciiDoc
    - use fast erb impl (erubis)

--- src/main/java/com/github/rmannibucau/blog/processor/ContentProcessor.java
+++ src/main/java/com/github/rmannibucau/blog/processor/ContentProcessor.java
@@ -13,10 +13,10 @@ import java.io.Serializable;
 @ApplicationScoped
 public class ContentProcessor implements Serializable {
     private static final String HTML = "Html";
-    private static final String MARDOWN = "Mardown";
-    private static final String ASCIIDOC = "Asciidoc";
+    private static final String MARKDOWN = "Markdown";
+    private static final String ASCIIDOC = "AsciiDoc";
 
-    private static final String[] FORMATS = new String[] { ASCIIDOC, HTML, MARDOWN };
+    private static final String[] FORMATS = new String[] { ASCIIDOC, HTML, MARKDOWN };
 
     private Asciidoctor asciidoctor;
 
@@ -28,13 +28,14 @@ public class ContentProcessor implements Serializable {
     public String toHtml(final String format, final String content) {
         if (HTML.equals(format)) {
             return content;
-        } else if (MARDOWN.equals(format)) {
+        } else if (MARKDOWN.equals(format)) {
             return new MarkdownProcessor().markdown(content);
         }
         // default == asciidoc
         return asciidoctor.render(content, OptionsBuilder.options()
-                .safe(SafeMode.SAFE).backend("html5").headerFooter(false)
-                .attributes(AttributesBuilder.attributes().attribute("icons!", "").asMap()).asMap());
+                .safe(SafeMode.SAFE).backend("html5").headerFooter(false).eruby("erubis")
+                .attributes(AttributesBuilder.attributes().attribute("icons!", "")
+                .attribute("source-highlighter", "coderay").asMap()).asMap());
     }
 
     public String[] getFormats() {
